0509208297c91864c5e984257c5cb2b940ebe7cf
jeire <43701707+jeire@users.noreply.github.com>
2024-12-19 15:57:19 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int CalcGCD(int num1, int num2)
+{
+    if (num1 < num2)  // 항상 num1이 num2보다 크도록 변경
+    {
+        int tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+
+    int remain = -1;
+    while(num2 != 0)
+    {
+        remain = num1 % num2;
+        num1 = num2;
+        num2 = remain;
+    }
+    return num1;
+}
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
+    int numer = numer1 * denom2 + numer2 * denom1;
+    int denom = denom1 * denom2;
+    int gcd = CalcGCD(numer, denom);
+    vector<int> answer = vector<int> {numer / gcd, denom / gcd};
+    return answer;
+}
