a944770ab42443588391b6b4761f121ed31df152
jeire <43701707+jeire@users.noreply.github.com>
2024-12-20 14:05:24 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.32 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
+++ C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -2,16 +2,7 @@ using System;
 
 public class Solution {
     public int[] solution(int[] num_list) {
-        int i2 = num_list.Length - 1;
-        int tmp = -1;
-        for (int i1 = 0; i1 < i2;)
-        {
-            tmp = num_list[i1];
-            num_list[i1] = num_list[i2];
-            num_list[i2] = tmp;
-            i1++;
-            i2--;
-        }
+        Array.Reverse(num_list);
         return num_list;
     }
 }
