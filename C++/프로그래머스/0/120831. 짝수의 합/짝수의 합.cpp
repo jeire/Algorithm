ac65cb4970ca30f744705bfc4dd4229218bb8d02
jeire <43701707+jeire@users.noreply.github.com>
2024-12-20 13:40:23 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int solution(int n) {
+    if (n % 2 != 0)
+        {
+            n = n - 1;
+        }
+        
+        int answer = 0;
+        while (n > 0)
+        {
+            answer += n;
+            n -= 2;
+        }
+        return answer;
+}
