55a5fcac815576db7e9e5f9df0fe27a6c2e903d8
jeire <43701707+jeire@users.noreply.github.com>
2024-12-20 13:39:57 +0900

    [level 0] Title: 짝수의 합, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        if (n % 2 != 0)
+        {
+            n = n - 1;
+        }
+        
+        int answer = 0;
+        while (n > 0)
+        {
+            answer += n;
+            n -= 2;
+        }
+        return answer;
+    }
+}
