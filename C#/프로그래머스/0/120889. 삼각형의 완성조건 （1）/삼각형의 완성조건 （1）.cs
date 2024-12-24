835723799af473e1cf38560c194431d294972b2b
jeire <43701707+jeire@users.noreply.github.com>
2024-12-24 11:14:07 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
+++ C#/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -2,7 +2,14 @@ using System;
 
 public class Solution {
     public int solution(int[] sides) {
-        Array.Sort(sides);
-        return sides[0] + sides[1] > sides[2] ? 1 : 2;
+        int sum = sides[0] + sides[1] + sides[2];
+        foreach (int side in sides)
+        {
+            if((sum - 1) / 2 + 1 <= side)
+            {
+                return 2;
+            }
+        }
+        return 1;
     }
 }
