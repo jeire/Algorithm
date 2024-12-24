41d5e69053e3f18e162a172e5a506c14495c7fd5
jeire <43701707+jeire@users.noreply.github.com>
2024-12-24 11:16:55 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int i = 1;
+        while (i * 6 % n != 0)
+        {
+            i++;
+        }
+        return i;
+    }
+}
