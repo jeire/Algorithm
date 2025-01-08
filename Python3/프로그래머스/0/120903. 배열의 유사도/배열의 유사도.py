4d27b1923b25f55829fcad89eb9cc91b0b04a2c4
jeire <43701707+jeire@users.noreply.github.com>
2025-01-08 17:33:03 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,2 @@
 def solution(s1, s2):
-    answer = 0
-    for key1 in s1:
-        for key2 in s2:
-            if key1 == key2:
-                answer += 1
-    return answer
+    return len(set(s1)&set(s2));
