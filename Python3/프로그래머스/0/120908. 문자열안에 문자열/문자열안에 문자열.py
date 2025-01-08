97ad6d0fd33685e2d6ca861b4f3f860d91b7beb3
jeire <43701707+jeire@users.noreply.github.com>
2025-01-08 17:53:38 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ Python3/프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,16 +1,2 @@
 def solution(str1, str2):
-    i = 0
-    j = 0
-    answer = 2
-    while i < len(str1):
-        if str1[i] == str2[j]:
-            j += 1
-            if j == len(str2):
-                answer = 1
-                break;
-        else:
-            answer = 2
-            j = 0
-        i += 1           
-    
-    return answer
+    return 1 + int(str2 not in str1)
