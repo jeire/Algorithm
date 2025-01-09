16476c5d57af3042dadc1585572dca5ebc979102
jeire <43701707+jeire@users.noreply.github.com>
2025-01-09 17:50:54 +0900

    Update 배열의 유사도.py

--- Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,3 +1,4 @@
 def solution(s1, s2):
-    # set는 겹치는 요소가 없는 list라고 생각하면 됩니다.
+    # set는 python에서 집합 연산을 할 수 있게 해주는 자료구조입니다.
+    # 문제는 교집합을 구하는 문제이므로 set를 쓰면 교집합을 구하는 연산을 할 수 있죠
     return len(set(s1)&set(s2));
