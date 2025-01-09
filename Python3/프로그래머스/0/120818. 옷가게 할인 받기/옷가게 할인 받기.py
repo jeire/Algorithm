b2d48e06f74a46477c079bcbece5c9c5a5829581
jeire <43701707+jeire@users.noreply.github.com>
2025-01-09 17:34:43 +0900

    Update 옷가게 할인 받기.py

--- Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,4 +1,6 @@
 def solution(price):
+    # 조건문의 조건을 겹치지 않게 써야 합니다. 안그러면 앞에서 다 걸려서 뒤의 조건들이 무의미해져요
+    # 연산자를 헷갈리지 않게 조심합니다. & 와 and를 헷갈릴 수 있어요
     if price >= 500000:
         return int(price * 0.8)
     elif price >= 300000:
