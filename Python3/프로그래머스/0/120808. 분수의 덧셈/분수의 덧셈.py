d14cb95fce58c633b5734146b924c989888842e3
jeire <43701707+jeire@users.noreply.github.com>
2025-01-09 17:30:20 +0900

    Update 분수의 덧셈.py

--- Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -4,15 +4,17 @@ def CalcGCD(num1, num2):
         num1 = num2
         num2 = tmp
     remain = -1
-    while num2 != 0:
+    while num2 != 0:  # while문은 최대공약수를 구하는 알고리즘입니다.
         remain = num1 % num2
         num1 = num2
         num2 = remain
     return num1
 
 def solution(numer1, denom1, numer2, denom2):
+    # 일단 기약분수를 고려하지 않고 통분을 먼저 진행합니다.
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
-    gcd = CalcGCD(numer, denom)
-    answer = [numer / gcd, denom / gcd]
+
+    gcd = CalcGCD(numer, denom)          # 기약분수를 만들기 위해 분모와 분자의 최대공약수를 구합니다.
+    answer = [numer / gcd, denom / gcd]  # 최대공약수로 분모와 분자를 나누면 됩니다.
     return answer
