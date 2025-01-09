062e3cfc81d6a21259445956e8f76fd2a97ab5a1
jeire <43701707+jeire@users.noreply.github.com>
2025-01-09 17:33:05 +0900

    Update 중앙값 구하기.py

--- Python3/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ Python3/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,2 +1,7 @@
 def solution(array):
+    """
+    list를 정렬하는 함수는 sorted(리스트 이름)라는 함수가 있습니다.
+    중앙값을 구하는 것이므로 list의 주소는 0부터 n까지일 것이고
+    가운데의 주소는 n입니다.
+    즉 전체 길이를 len()으로 구한 뒤 2로 나누면 되겠죠.
     return sorted(array)[len(array) // 2]
