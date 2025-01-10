7f561774433e4bbd8d468a949b7a7d312ba687ff
jeire <43701707+jeire@users.noreply.github.com>
2025-01-10 13:37:33 +0900

    add comment

--- Python3/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ Python3/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,6 @@
 def solution(array, height):
     array.sort()    # list를 정렬합니다
     for i in range(len(array)):
-        if array[i] > height:
-            return len(array) - i
-    return 0
+        if array[i] > height:      # 머쓱이보다 키가 큰 사람이 나오면
+            return len(array) - i  # 전체 list의 길이에서 해당 index를 빼면 됩니다.
+    return 0  # 머쓱이보다 키가 큰 사람이 없으면 반복문을 끝까지 실행하게 되므로 0명입니다
