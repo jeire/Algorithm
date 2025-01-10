08ac9c237e686487aeadc28c915f585cc1f0b70b
jeire <43701707+jeire@users.noreply.github.com>
2025-01-10 13:30:56 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ Python3/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -3,6 +3,6 @@ def double_value(n):
     return n * 2
 
 def solution(numbers):
-    """각 원소를 두 배로 만든 배열을 반환합니다."""
-    doubled_numbers = list(map(double_value, numbers))  # double_value 함수를 사용하여 각 원소 두 배
-    return doubled_numbers  # 결과 리스트 반환
+    for i in range(len(numbers)):  # 배열의 처음부터 끝까지 index로 접근합니다.
+        numbers[i] *= 2            # 배열의 각 요소에 2를 곱하여 다시 요소에 덮어씁니다.
+    return numbers                 # 결과 리스트 반환
