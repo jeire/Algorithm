abcf7246d86d527b0d27be73bb9923491a6810e4
jeire <43701707+jeire@users.noreply.github.com>
2025-01-10 13:47:25 +0900

    Update 짝수의 합.py

--- Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,5 +1,5 @@
 def solution(n):
-    answer = 0
-    for i in range(2, n + 1, 2):
-        answer += i
+    answer = 0  # 더한 값을 저장할 변수 0으로 초기화
+    for i in range(2, n + 1, 2):  # 짝수는 2에서 시작해서 2씩 증가합니다. 그리고 n이 짝수이면 n도 포함해야 합니다
+        answer += i               # 기존 합에 새로운 짝수 더하기
     return answer
