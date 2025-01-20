d6a16d4b1d25f2af4cf3cd2cca61920a57ff5d49
jeire <43701707+jeire@users.noreply.github.com>
2025-01-20 15:02:02 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
+++ C#/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
@@ -2,19 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int[] dot) {
-        if (dot[0] > 0)
-        {
-            if (dot[1] > 0)
-                return 1;
-            else
-                return 4;
-        }
-        else
-        {
-            if (dot[1] > 0)
-                return 2;
-            else
-                return 3;
-        }
+        return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
     }
 }
