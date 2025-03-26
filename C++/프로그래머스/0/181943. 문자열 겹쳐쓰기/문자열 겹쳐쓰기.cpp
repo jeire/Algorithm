9e98ec07345d8bc24e25c98c387e5e70c0403325
jeire <43701707+jeire@users.noreply.github.com>
2025-03-26 17:29:56 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- C++/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ C++/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -4,6 +4,5 @@
 using namespace std;
 
 string solution(string my_str, string overwrite_str, int s) {
-    string answer = my_str.substr(0, s) + overwrite_str + my_str.substr(s + overwrite_str.size());
-    return answer;
+    return my_str.substr(0, s) + overwrite_str + my_str.substr(s + overwrite_str.size());
 }
