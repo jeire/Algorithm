b60b9fde18941d1d676e0f852404ff439f13f733
jeire <43701707+jeire@users.noreply.github.com>
2025-05-30 10:11:21 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] ss = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(ss[0]) + int.Parse(ss[1]));
+        }
+    }
+}
