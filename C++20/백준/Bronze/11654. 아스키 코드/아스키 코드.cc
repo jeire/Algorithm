5e7b5f2e389692db1fb985fbe16fb1565707f3ce
jeire <43701707+jeire@users.noreply.github.com>
2025-05-30 10:28:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a;
+    char ss;
+    cin >> ss;
+    cout << (int)ss;
+    return 0;
+}
