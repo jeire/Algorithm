cb017713665654b1bea74be3efe8f986591c272a
jeire <43701707+jeire@users.noreply.github.com>
2025-07-29 11:49:55 +0900

    [Bronze V] Title: A-B, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        int[] a = s.Length > 0 ? Array.ConvertAll(s, int.Parse) : new int[2];
+        Console.WriteLine(a[0] - a[1]);
+    }
+}
