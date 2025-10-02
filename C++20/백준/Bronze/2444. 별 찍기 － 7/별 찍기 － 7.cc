2aeb3c5f19757cac20c2e47d5307f89650da8965
jeire <43701707+jeire@users.noreply.github.com>
2025-10-02 12:19:10 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            cout << " ";
+        }
+
+        int odd = 2 * i + 1;
+        for (int j = 0; j < odd; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        int odd = 2 * (n - i) - 1;
+        for (int j = odd; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
