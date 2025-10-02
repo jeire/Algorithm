f82f8448337be06b3729655c630b16f71277bf87
jeire <43701707+jeire@users.noreply.github.com>
2025-10-02 09:14:05 +0900

    [Bronze II] Title: 숫자, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ C++20/백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long a, b, temp;
+    cin >> a >> b;
+
+
+    if (a > b)
+    {
+        temp = b;
+        b = a;
+        a = temp;
+    }
+
+    if (b - a < 2)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << b - a - 1 << "\n";
+        for (int i = a + 1; i < b; i++)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
