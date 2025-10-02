6f7489da19f9168c603c288f7f02d60605617e3f
jeire <43701707+jeire@users.noreply.github.com>
2025-10-02 11:54:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ C++20/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -6,12 +6,12 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a;
-    cin >> a;
+    int n;
+    cin >> n;
 
-    for (int i = 1; i <= a; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 0; j <= i; j++)
         {
             cout << "*";
         }
